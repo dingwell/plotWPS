31f1870c4869ea98c9f2bd4e9cbb9037e8a10b9b
Adam Dingwell <publicadam2011@gmail.com>
2014-05-23 14:28:22 +0200

    Added shell script for plotting landsea from commandline

---
+++ run_plot_landsea.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Tool to create plot using "plot_landsea.ncl" from the command-line
+#
+# Installation:
+# 1. Put this script in your system path or in the desired working directory
+# 2. Modify the PATH_TO_PLOT_LANDSEA variable to point to the location of
+#    the plot_landsea.ncl
+# 
+# Usage examples:
+#   run_plot_landsea.sh file1 file2 [file3 [...]]
+#
+#   run_plot_landsea.sh subfolder/geo_em*.nc
+
+PATH_TO_PLOT_LANDSEA=$HOME/programming/git/plotWPS/
+
+export FILES=$@
+
+ncl "$PATH_TO_PLOT_LANDSEA/plot_landsea.ncl"
+
+
